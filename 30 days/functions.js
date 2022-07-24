52ea8fa8730a5a45c7432bcebd4a5ed5cdf97b79
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-07-24 13:46:51 +0530

    functions

--- 30 days/functions.js
+++ 30 days/functions.js
@@ -30,48 +30,59 @@
 // Write a function generateColors which can generate any number 
 // of hexa or rgb colors.
 
-function randChar(str){
-        return str.charAt(Math.floor(Math.random() *str.length));
-    }
+// function randChar(str){
+//         return str.charAt(Math.floor(Math.random() *str.length));
+//     }
     
-const characters = 'abcdef0123456789';
-function genHexa(){
-    strhex = '#';
-    for(var i=0;i<6;i++){
-        strhex += randChar(characters);
-    }
-    return strhex;
-}
+// const characters = 'abcdef0123456789';
+// function genHexa(){
+//     strhex = '#';
+//     for(var i=0;i<6;i++){
+//         strhex += randChar(characters);
+//     }
+//     return strhex;
+// }
 
-function genRGB(){
-    strrgb = 'rgb(';
-    for(var i=0;i<3;i++){
-        strrgb = strrgb + (Math.floor(Math.random()*256)).toString()
-        if(i===2)
-            strrgb+=")"
-        else
-            strrgb+=", "
-    }
-    return strrgb
-}
+// function genRGB(){
+//     strrgb = 'rgb(';
+//     for(var i=0;i<3;i++){
+//         strrgb = strrgb + (Math.floor(Math.random()*256)).toString()
+//         if(i===2)
+//             strrgb+=")"
+//         else
+//             strrgb+=", "
+//     }
+//     return strrgb
+// }
 
-type = window.prompt("1 for hexa, 2 for rgb")
-quantity = window.prompt("number of colors required")
+// type = window.prompt("1 for hexa, 2 for rgb")
+// quantity = window.prompt("number of colors required")
 
-collist = []
-generateColors=(type,quantity)=>{
-    if(type==="1"){
-        for(let i =0;i<quantity;i++){
-            collist.push(genHexa());
-        }
-    }
-    else if(type==="2"){
-        for(let i =0;i<quantity;i++){
-            collist.push(genRGB());
-        }
-    }
-    return collist;
-}
+// collist = []
+// generateColors=(type,quantity)=>{
+//     if(type==="1"){
+//         for(let i =0;i<quantity;i++){
+//             collist.push(genHexa());
+//         }
+//     }
+//     else if(type==="2"){
+//         for(let i =0;i<quantity;i++){
+//             collist.push(genRGB());
+//         }
+//     }
+//     return collist;
+// }
 
-generateColors(type,quantity);
-console.log(collist);
+// generateColors(type,quantity);
+// console.log(collist);
+
+//--------------------------------------------------------------//
+
+const sumAllNums = (...args) => {
+    sum = 0;
+    for(a of args)
+    sum+=a;
+    return sum;
+   }
+
+console.log(sumAllNums(1,2,3,4,5,6,7,8,9,"",10)); //4510
